25c29ec40d9d815d5102ce0c90e4b87c9e0bfcf3
hehh-random <jiyabhandari767@gmail.com>
2026-03-18 13:53:35 +1300

    js

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 function celebrate(){
-
-  // Hide buttons instead of removing
   document.querySelectorAll("button").forEach(btn=>{
     if(btn !== restartBtn){
-      btn.style.display = "none";
+      btn.remove();
     }
   });
 
   for(let i=0;i<80;i++){
-    createHeart(
-      Math.random()*window.innerWidth,
-      Math.random()*window.innerHeight
-    );
+    createHeart(Math.random()*window.innerWidth, Math.random()*window.innerHeight);
   }
 
   finalMsg.style.display = "block";
